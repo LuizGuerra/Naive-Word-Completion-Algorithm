787db6ecaf71b3e7cc887bdda38ef59b0b6412c3
LuizGuerra <luizpedrofg@gmail.com>
2018-12-02 01:38:14 -0200

    Added the code in ReadCSVFile and Created a Main class

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        ReadCSVFile read_it = new ReadCSVFile();
+        read_it.read();
+    }
+}
--- src/ReadCSVFile.java
+++ src/ReadCSVFile.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
+// package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class ReadCSVFile {
+
+    private String path, line, split_on;
+    private BufferedReader br;
+
+    public ReadCSVFile () {
+        this.path = "/nomes.csv";
+        this.br = null;
+        this.line = "";
+        this.split_on = ";";
+    }
+
+    public void read () {
+        try {
+            br = new BufferedReader(new FileReader(path));
+            while ((line = br.readLine()) != null) {
+                String [] name = line.split(split_on);
+                System.out.println("Name [" + name[4] + " , meaning = " + name[5] + "]");
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 }
