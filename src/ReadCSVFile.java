52a13352dc9207f3a4236eec2b55889030667e8a
LuizGuerra <luizpedrofg@gmail.com>
2018-12-02 01:45:29 -0200

    Added a relative Path!!

--- src/ReadCSVFile.java
+++ src/ReadCSVFile.java
@@ -11,7 +11,7 @@ public class ReadCSVFile {
 
     public ReadCSVFile () {
         // You will have to change the path name in your desktop
-        this.path = "C:\\Users\\Celito\\Desktop\\T3_Algoritmo_I\\src\\nomes.csv";
+        this.path = "src\\nomes.csv";
         this.br = null;
         this.line = "";
         this.split_on = ";";
@@ -22,7 +22,7 @@ public class ReadCSVFile {
             br = new BufferedReader(new FileReader(path));
             while ((line = br.readLine()) != null) {
                 String [] name = line.split(split_on);
-                System.out.println("Name [" + name[0] + " , meaning = " + name[1] + "]");
+                System.out.println(name[0] + "\t\t" + name[1]);
             }
 
         } catch (FileNotFoundException e) {
