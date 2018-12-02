be4ecc118f21124d711fa9c908ae82ab73c7a94d
LuizGuerra <luizpedrofg@gmail.com>
2018-12-02 02:49:10 -0200

    Added a way on how to get the information in the App.java class

--- src/App.java
+++ src/App.java
@@ -1,6 +1,12 @@
 public class App {
     public static void main(String[] args) {
         ReadCSVFile read_it = new ReadCSVFile();
-        read_it.read();
+        String [][] information = read_it.read();
+        Integer size = read_it.getFile_size();
+
+        for (int i = 0; i < size; i++) {
+            System.out.println(information[i][0] + "\t\t" + information[i][1]);
+        }
+
     }
 }
--- src/ReadCSVFile.java
+++ src/ReadCSVFile.java
@@ -1,13 +1,20 @@
 // package PACKAGE_NAME;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.LineNumberReader;
+
+import java.util.List;
+import java.util.ArrayList;
 
 public class ReadCSVFile {
 
     private String path, line, split_on;
     private BufferedReader br;
+    private Integer file_size;
+    //private
 
     public ReadCSVFile () {
         // You will have to change the path name in your desktop
@@ -15,16 +22,44 @@ public class ReadCSVFile {
         this.br = null;
         this.line = "";
         this.split_on = ";";
+        this.file_size = file_size();
     }
 
-    public void read () {
-        try {
-            br = new BufferedReader(new FileReader(path));
-            while ((line = br.readLine()) != null) {
-                String [] name = line.split(split_on);
-                System.out.println(name[0] + "\t\t" + name[1]);
-            }
+    public Integer getFile_size(){
+        return this.file_size;
+    }
 
+    /**
+     * Here, the method evaluates the amount of lines that the file have.
+     */
+    private Integer file_size() {
+        Integer fs = 0;
+        try {
+            LineNumberReader lnr = new LineNumberReader(new FileReader(path));
+            lnr.skip(Long.MAX_VALUE);
+            fs = lnr.getLineNumber();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return fs;
+    }
+
+    /**
+     * Here the method reads the CSV file and return it as a 2 dimensions array.
+     */
+    public String [][] read () {
+        if (this.file_size == 0) {
+            return null;
+        }
+        String [][] information = new String[this.file_size][2];
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+        int aux = 0;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(split_on);
+                information[aux][0] = data[0];
+                information[aux][1] = data[1];
+                aux++;
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -38,6 +73,6 @@ public class ReadCSVFile {
                 }
             }
         }
+        return information;
     }
-
 }
